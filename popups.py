421b619d7b19c2378b1fbae7e4ca5ed7dab1eed3
szypkiwonsz <szypkiwonsz@github.com>
2020-03-21 13:59:45 +0100

    Added file with pop-ups window closing functions

---
+++ popups.py
@@ -0,0 +1,23 @@
+class TinderPopups:
+
+    def tinder_notifications(self):
+        notification_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/'
+                                                             'button[2]')
+        notification_btn.click()
+
+    def tinder_localization(self):
+        localization_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/'
+                                                             'button[1]')
+        localization_btn.click()
+
+    def keep_going(self):
+        keep_going_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        keep_going_btn.click()
+
+    def popup_start_screen(self):
+        keep_going_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        keep_going_btn.click()
+
+    def verify_email(self):
+        remind_later = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/div[2]/button[2]')
+        remind_later.click()
