1b204e2fd7cbae88d19bc98a4236a5b73e96ce1a
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-08 20:45:21 +0200

    Code fixes

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 class TinderFunctions:
 
     def swipe_right(self):
-        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/'
-                                                     'div[2]/div[4]/button')
+        like_btn = self.driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
         like_btn.click()
